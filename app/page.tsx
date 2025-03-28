5ea538438aa3e47211a42dda5e47d98b7e6eda8e
Victor okpaire <vixtorjason@gmail.com>
2025-03-28 15:09:41 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 export default function Home() {
   return (
-    <div className="text-4xl">
-      <h2>Hello Welcome to Hydra</h2>
+    <div>
+      <h1 className="text-3xl text-brand-100">Hello, welcome to Hydra.</h1>
     </div>
   );
 }
