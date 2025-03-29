0127357fd8e1a2ac86b7be12d2cff61db9700164
Victor okpaire <vixtorjason@gmail.com>
2025-03-29 23:53:55 +0100

    updated OTP verifiation + authentication

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -16,6 +16,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OtpModal from "./OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -149,6 +150,9 @@ const Authform = ({ type }: { type: FormType }) => {
         </form>
       </Form>
       {/* OTP Verification */}
+      {accountId && (
+        <OtpModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
